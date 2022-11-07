df63b06b3814ef5a662851dcabd4a9c27e9ecfcc
Luis <Hairchevere@gmail.com>
2022-11-07 13:28:52 -0600

    finished up the higscore java

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -153,3 +153,27 @@ var highscoresList = document.querySelector('#highscoresList');
 var backButton = document.querySelector('#backButton');
 var clearButton = document.querySelector('clear')
 
+function showScores(){
+  if (savedScore!== null) {
+    var scoreList = document.createElement('sl');
+    scoreList.className = "scorelistClass";
+    for( var i = 0; i < savedScore.length; i++) {
+      var initials = savedScore[i].inits;
+      var scores = savedScore[i].userinfo
+      var enteredScore = document.createElement('li');
+      enteredScore.innerHTML = initials + '-' + scores;
+      scoreList.appendChild(enderedScore);
+    }
+    highscoresList.appendChild(enderedScore);
+  }
+};
+
+showScores();
+backButton.addEventListener('click', function() {
+  location.href = "index.html";
+});
+
+clearButton.addEventListener('click', function(){
+  highscoresList.innerHTML = '';
+  window.localStorage.clear();
+}):
